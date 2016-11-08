4b1d7ff827ae1a2d50d527aa427a0734d4c44ee9
thb <52thb1314@gmail.com>
2016-11-08 14:03:03 +0800

    外部中断库

---
+++ mylib/inc/exit.h
@@ -0,0 +1,6 @@
+#ifndef __EXIT_H
+#define __EXIT_H
+#include "mytype.h"
+extern void InitExit(void);
+
+#endif
---
+++ mylib/src/exit.c
@@ -0,0 +1,50 @@
+#include "exit.h"
+#include "myport.h"
+#include "delay.h"
+#include <STC15F2K60S2.H>
+
+
+void InitExit(void)
+{
+	//开启外部中断0 1 2 3
+	IT0 = 1;
+	EX0 = 1;
+	
+	//外部中断 1
+	IT1 = 1;
+	EX1 = 1;
+	
+	// 使能外部中断 2
+	INT_CLKO |= 0x10;
+	
+	// 使能外部中断 3
+	INT_CLKO |= 0x20;
+	
+}
+
+void exint3() interrupt 11
+{
+	u8 tmp = NO_BTN_DOWN;
+	u8 temp_btn_state = NO_BTN_DOWN;
+	if(B_IS_START_BTN_DOWN())
+	{
+		temp_btn_state = START_BTN_DOWN;
+	}
+	if((button_state&(~BTN_UPDATE)) == temp_btn_state) 
+		return;
+	delay_ms(5);
+	if(B_IS_START_BTN_DOWN())
+	{
+		tmp = START_BTN_DOWN;
+	}
+	//如果和状态和原来的不同 说明是干扰
+	if(tmp != temp_btn_state)
+		return;
+	//更新按键状态
+	button_state = tmp;
+	SET_BTN_UPDATE(button_state);
+	
+
+}
+
+
