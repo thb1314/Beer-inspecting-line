8ead44a6d094f7ddc55852b196ce9dccbafee00a
thb <52thb1314@gmail.com>
2016-11-28 21:44:34 +0800

    主程序文件

--- main.c
+++ main.c
@@ -27,8 +27,17 @@
 #include "delay.h"
 
 
+//创建一个任务 检测传感器4是否连续被触发
+static u8 idata Sensor4TrigTime = 0;
+static u8 idata Sensor5TrigTime = 0;
 
 
+volatile Task idata TskIsSensor4_5_TrigOk;
+void TskIsSensor4_5_TrigOkCallBack(void*);
+bit is_serious_block = 0;
+
+#define SENSOR4TRIGTIME_MAX 50
+#define SENSOR5TRIGTIME_MAX 50
 
 void main()
 {
@@ -77,5 +86,72 @@ void SystemInit(void)
 	//开启总中断
 	SET_PORT(EA);
 	
+	
+
+	TskIsSensor4_5_TrigOk.timer = 100;
+	TskIsSensor4_5_TrigOk.type = TIME_TASK;
+	TskIsSensor4_5_TrigOk.isstart = 1;
+	TskIsSensor4_5_TrigOk.function = TskIsSensor4_5_TrigOkCallBack; 
+	TaskInit(&TskIsSensor4_5_TrigOk);
+	
 }
 
+void TskIsSensor4_5_TrigOkCallBack(void* ptr)
+{
+	Task* mytask = (Task*)(ptr);
+	if(IS_SENSOR_TRIGGER(I_SENSOR4))
+	{
+		if(Sensor4TrigTime <= SENSOR4TRIGTIME_MAX)
+		{
+			Sensor4TrigTime++;
+		}
+	}
+	else
+	{
+		if(IS_HIGH(is_serious_block))
+		{
+			//write 严重报警解除
+			STOP_SERIOUS_BLOCK_WARNING();
+			// 开始剔除操作
+		}
+			
+		Sensor4TrigTime = 0;
+		CLR_PORT(is_serious_block);
+	}
+	if( Sensor4TrigTime == SENSOR4TRIGTIME_MAX )
+	{
+		//write 执行严重阻塞报警
+		OPEN_SERIOUS_BLOCK_WARNING();
+		//停止剔除操作
+		
+		//发送给上位机
+		
+		
+		SET_PORT(is_serious_block);
+	}
+	
+	
+	
+	if(IS_SENSOR_TRIGGER(I_SENSOR5))
+	{
+		if(Sensor5TrigTime <= SENSOR5TRIGTIME_MAX)
+		{
+			Sensor5TrigTime++;
+		}
+	}
+	else
+	{
+		// write 传给上位机
+
+		STOP_BLOCK_WARNING();	
+		Sensor5TrigTime = 0;
+
+	}
+	if( Sensor5TrigTime == SENSOR5TRIGTIME_MAX )
+	{
+
+		OPEN_BLOCK_WARNING();
+		// write 传给上位机
+		
+	}
+}
